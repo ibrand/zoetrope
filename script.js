2e1a0069e8c8400b9131ae176464c000647d68c1
ibrand <me@ilonabrand.com>
2015-08-25 11:16:48 -0400

    Displays the first sprite to the center of the canvas

---
+++ script.js
@@ -0,0 +1,36 @@
+var canvas = document.getElementById('canvas'),
+    ctx = canvas.getContext('2d');
+
+canvas.width = 400;
+canvas.height = 400;
+
+// first figure out how big the spritesheet is (pixel dimensions)
+var spritesheet = new Image();
+spritesheet.src = "images/MuybridgeSpritesheet.gif";
+
+// onload waits until the image is loaded before trying to access it's attributes
+spritesheet.onload = function() {
+    // divide both dimensions by 4 to get the width and height of the indivudual sprites
+    var spriteWidth = spritesheet.width/4;
+    var spriteHeight = spritesheet.height/4;
+
+    // State variables for drawing the sprite
+    var startX = 0,
+        startY = 0,
+        endX = spriteWidth,
+        endY = spriteHeight;
+
+    console.log("IMG: "+spritesheet);
+    console.log("width: "+spritesheet.width);
+    console.log("height: "+spritesheet.height);
+
+    // Center the sprite on the canvas
+    spritePositionX = canvas.width/2 - spriteWidth/2;
+    spritePositionY = canvas.height/2 - spriteHeight/2;
+
+    var drawSprite = function() {
+        ctx.drawImage(spritesheet, startX, startY, endX, endY, spritePositionX, spritePositionY, spriteWidth, spriteHeight);
+    }
+
+    drawSprite();
+}
